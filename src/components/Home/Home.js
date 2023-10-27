1e16d7c57648d79f2142a25dd961ae19ea63dabc
likithsarma <144110548+likithsarma@users.noreply.github.com>
2023-10-27 22:30:24 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -20,7 +20,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name">GUDA VENKATA RAMANAIAH </strong>
+                <strong className="main-name">JANDHYALA LIKITH SARMA </strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
